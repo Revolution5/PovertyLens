366dcc52a6fb3c794aad7f1269c34eb1c74fbb18
Mar-ty04 <marisol.morales0309@gmail.com>
2025-11-22 21:09:30 -0800

    modify backend and frontend to display data

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,33 @@ app.get('/', async (req, res) => {
   }
 })
 
+// Setting up code for button counter
+app.get('/api/counter', async (req, res) => {
+  try {
+    let counter = await db.collection('counters').findOne({ name: 'button-clicks' })
+    res.json({ count: counter.count })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+app.post('/api/counter/increment', async (req, res) => {
+  try {
+    const result = await db.collection('counters').findOneAndUpdate(
+      { name: 'button-clicks' },              // Find this document
+      { $inc: { count: 1 } },                 // Add 1 to count
+      { upsert: true, returnDocument: 'after' } // Return the new value
+    )
+    
+    console.log(`Total clicks: ${result.count}`) // Log to server console
+    res.json({ count: result.count })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
 app.listen(port, async () => {
   await connectDB()
   console.log(`Server listening on port ${port}`)
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,60 @@
 "use client";
 import Image from "next/image";
+import {useState, useEffect} from "react";
 
 export default function Home() {
-    const handleClick = async () => {
-    try {
-      const res = await fetch('http://localhost:4000', { method: 'GET' })
-      const data = await res.json()
-      console.log(data)
-    } catch (err) {
-      console.error('Fetch error:', err)
-    }
-  }
+    const [count, setCount] = useState(0)
+
+    // Load counter when page loads
+    useEffect(() => {
+    fetch("http://localhost:4000/api/counter")
+      .then(res => res.json())
+      .then(data => setCount(data.count));
+  }, []);
+
+  // Increase the counter
+  const handleClick = () => {
+    fetch("http://localhost:4000/api/counter/increment", { method: "POST" })
+      .then(res => res.json())
+      .then(data => setCount(data.count));
+  };
+
   return (
-    <div>
-      <button onClick={handleClick}>
+    <div style={{ 
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: "40px",
+      gap: "20px"
+    }}>
+
+      <button onClick={handleClick}
+      style={{
+        backgroundColor: "#AC7F5E",
+        color: "#371c01ff",
+        fontSize: "30px",
+        cursor: "pointer",
+        borderRadius: "12px",
+        padding: "12px 24px",
+        
+      }}>
       Click me
       </button>
+
+    <div style= {{
+      fontSize: "25px",
+      color: "#371c01ff"
+    }}>
+      Total amount of clicks overall: 
     </div>
+
+    <div style={{ 
+      fontSize: "48px",
+       fontWeight: "bold",
+       color: "#AC7F5E" 
+    }}>
+      {count}
+    </div>
+  </div>
   );
 }
