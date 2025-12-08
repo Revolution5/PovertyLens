83cb33a348ac755e2b50f186041a0691a6022c2e
Mar-ty04 <marisol.morales0309@gmail.com>
2025-12-08 14:00:48 -0800

    Create page.tsx

---
+++ frontend/app/AboutUs/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutUs() {
+    return (
+        <div className="min-h-screen">
+            <h1 className="pt-10 pb-1 text-center text-[70px] text-[#623100] font-black">
+                About Us
+            </h1>
+        </div>
+            
+    );
+}
