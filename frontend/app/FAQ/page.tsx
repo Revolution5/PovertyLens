562dd233e30e9f13f86137fc974258a285c75992
Mar-ty04 <marisol.morales0309@gmail.com>
2025-12-08 13:56:11 -0800

    Create FAQ page

---
+++ frontend/app/FAQ/page.tsx
@@ -0,0 +1,9 @@
+export default function FAQ(){
+    return (
+        <div className="min-h-screen">
+            <h1 className="pt-10 pb-1 text-center text-[70px] text-[#623100] font-black">
+                Frequently Asked Questions
+            </h1>
+        </div>
+    )
+}
